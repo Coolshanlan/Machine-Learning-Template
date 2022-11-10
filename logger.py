e59780d71d1af11218ecdf57188516b2cc3dd308
Coolshanlan <coolshanlan@gmail.com>
2022-11-11 00:31:11 +0800

    [update] logger redesign

--- logger.py
+++ logger.py
@@ -28,19 +28,20 @@ def create_subtitle(fig: plt.Figure, grid: SubplotSpec, title: str):
     row.axis('off')
 
 class Logger:
-    logger_dict={}
-    log_category=set([])
     save_dir='logger_dir'
     config=None
+    loaded=False
     experiment_name=None
     history={'records':pd.DataFrame(columns=['experiment_name','epoch','tag']),
              'configs':{}}
 
     @staticmethod
-    def init(experiment_name):
+    def init(experiment_name,overwrite=False):
         Logger.config=Config()
         Logger.experiment_name=experiment_name
-        Logger.history[Logger.experiment_name]={}
+        if experiment_name in Logger.history['records'].experiment_name.unique() and not overwrite:
+            raise Exception(f'{experiment_name} experiment name already exist, please rename or set overwrite parameter to True.')
+        Logger.history['records'] = Logger.history['records'][Logger.history['records'].experiment_name != experiment_name]
 
     def __init__(self,tag):
         if not Logger.experiment_name:
@@ -48,21 +49,17 @@ class Logger:
 
         self.tag=tag
         self.record=pd.DataFrame()
-        Logger.logger_dict[self.tag] = pd.DataFrame()
         self.epoch=0
         if not os.path.exists(Logger.save_dir):
             os.mkdir(Logger.save_dir)
 
-
-    def update_category(self,new_row):
-        Logger.log_category.update(set(new_row.columns.to_list()))
-
     def __call__(self,**kwargs):
         new_row = pd.DataFrame(kwargs,index=[self.epoch],dtype=np.float64)
-        self.update_category(new_row)
+        new_row['tag']=self.tag
+        new_row['experiment_name']=self.experiment_name
         new_row['epoch']=self.epoch
         self.record=pd.concat([self.record,new_row])
-        Logger.logger_dict[self.tag]=self.record
+        Logger.history['records'] = pd.concat([Logger.history['records'],self.record])
         self.epoch+=1
 
     def get_last_record(self):
@@ -76,62 +73,62 @@ class Logger:
         best_index,best_record = self.get_best_record(category,mode)
         return (len(self.record)-1)==best_index
 
+    # @staticmethod
+    # def plot(show_tag=None,
+    #          show_category=None,
+    #          figsize=(7.6*1.5,5*1.5),
+    #          cmp=mpl.cm.Set2.colors,
+    #          ylim={},
+    #          filename='logger_history.png',
+    #          save=True,
+    #          show=True):
+
+    #     if not show_tag:
+    #         show_tag = Logger.logger_dict.keys()
+
+    #     exist_category=set([])
+    #     for logger_tags in show_tag:
+    #         exist_category.update(Logger.logger_dict[logger_tags].drop(columns=['epoch']))
+
+    #     if show_category:
+    #         show_category = [c for c in show_category if c in exist_category]
+    #     else:
+    #         show_category = exist_category
+
+    #     fig, axs = plt.subplots(1,len(show_category),figsize=(len(show_category)*figsize[0]+len(show_category)*0.25,figsize[1]))#,constrained_layout=True)
+    #     plt.ticklabel_format(style='plain', axis='x', useOffset=False)
+
+    #     axs = np.array(axs).flatten()
+
+    #     for lidx,logger_tags in enumerate(show_tag):
+    #         plot_color = cmp[lidx]
+    #         for cidx,c in enumerate(show_category):
+    #             if c in Logger.logger_dict[logger_tags]:
+    #                 _record=Logger.logger_dict[logger_tags]
+    #                 _record = _record.reset_index(drop=True)
+    #                 axs[cidx].plot(range(len(_record)),_record[c],label=logger_tags,color=plot_color,linewidth=2)
+    #                 axs[cidx].set_title('{}'.format(c), fontsize=20)
+    #                 axs[cidx].legend(loc='upper left',fontsize=15)
+    #                 axs[cidx].tick_params(axis='both', labelsize=15)
+    #                 axs[cidx].grid(axis='y', linestyle='-', alpha=0.7,color='lightgray')
+    #                 axs[cidx].xaxis.set_major_locator(MaxNLocator(integer=True))
+    #                 if c in ylim.keys():
+    #                     axs[cidx].set_ylim(ylim[c][0],ylim[c][1])
+    #     fig.suptitle(Logger.experiment_name,fontsize=22)
+    #     plt.tight_layout()
+    #     if save:
+    #         plt.savefig(os.path.join(Logger.save_dir,filename))
+    #     if show:
+    #         plt.show()
+    #     plt.close()
+
     @staticmethod
-    def plot(show_tag=None,
+    def plot(show_experiment=None,show_tag=None,
              show_category=None,
              figsize=(7.6*1.5,5*1.5),
              cmp=mpl.cm.Set2.colors,
              ylim={},
-             filename='logger_history.png',
-             save=True,
-             show=True):
-
-        if not show_tag:
-            show_tag = Logger.logger_dict.keys()
-
-        exist_category=set([])
-        for logger_tags in show_tag:
-            exist_category.update(Logger.logger_dict[logger_tags].drop(columns=['epoch']))
-
-        if show_category:
-            show_category = [c for c in show_category if c in exist_category]
-        else:
-            show_category = exist_category
-
-        fig, axs = plt.subplots(1,len(show_category),figsize=(len(show_category)*figsize[0]+len(show_category)*0.25,figsize[1]))#,constrained_layout=True)
-        plt.ticklabel_format(style='plain', axis='x', useOffset=False)
-
-        axs = np.array(axs).flatten()
-
-        for lidx,logger_tags in enumerate(show_tag):
-            plot_color = cmp[lidx]
-            for cidx,c in enumerate(show_category):
-                if c in Logger.logger_dict[logger_tags]:
-                    _record=Logger.logger_dict[logger_tags]
-                    _record = _record.reset_index(drop=True)
-                    axs[cidx].plot(range(len(_record)),_record[c],label=logger_tags,color=plot_color,linewidth=2)
-                    axs[cidx].set_title('{}'.format(c), fontsize=20)
-                    axs[cidx].legend(loc='upper left',fontsize=15)
-                    axs[cidx].tick_params(axis='both', labelsize=15)
-                    axs[cidx].grid(axis='y', linestyle='-', alpha=0.7,color='lightgray')
-                    axs[cidx].xaxis.set_major_locator(MaxNLocator(integer=True))
-                    if c in ylim.keys():
-                        axs[cidx].set_ylim(ylim[c][0],ylim[c][1])
-        fig.suptitle(Logger.experiment_name,fontsize=22)
-        plt.tight_layout()
-        if save:
-            plt.savefig(os.path.join(Logger.save_dir,filename))
-        if show:
-            plt.show()
-        plt.close()
-
-    @staticmethod
-    def plot_experiment(show_experiment=None,show_tag=None,
-             show_category=None,
-             figsize=(7.6*1.5,5*1.5),
-             cmp=mpl.cm.Set2.colors,
-             ylim={},
-             filename='experiments_history.png',
+             filename='logger_plot.png',
              save=True,
              show=True):
 
@@ -196,7 +193,7 @@ class Logger:
         plt.close()
 
     @staticmethod
-    def plot_multi_experiment(show_experiment=None,show_tag=None,
+    def plot_experiments(show_experiment=None,show_tag=None,
              show_category=None,
              figsize=(7.6*1.5,5*1.5),
              cmp=mpl.cm.Set1.colors,
@@ -212,7 +209,7 @@ class Logger:
             show_experiment=show_experiment[0]
 
         if isinstance(show_experiment,str):
-            Logger.plot_experiment(show_experiment=show_experiment,show_tag=show_tag,
+            Logger.plot(show_experiment=show_experiment,show_tag=show_tag,
              show_category=show_category,
              figsize=figsize,
              cmp=cmp,
@@ -235,7 +232,7 @@ class Logger:
         if show_category:
             show_category = [c for c in show_category if c in exist_category]
         else:
-            show_category=exist_category
+            show_category=sorted(exist_category)
 
         fig, axs = plt.subplots(len(show_tag),len(show_category),figsize=(len(show_category)*figsize[0]+len(show_category)*0.25,(figsize[1]+1)*len(show_tag)))#,constrained_layout=True)
         plt.ticklabel_format(style='plain', axis='x', useOffset=False)
@@ -274,22 +271,12 @@ class Logger:
             del Logger.history['configs'][experiment_name]
 
     @staticmethod
-    def export_logger(dir_path=None,filename='logger_history',read_if_exist=True,overwrite_if_exist=True):
+    def export_logger(dir_path=None,filename='logger_history',overwrite=False):
         if not dir_path: dir_path = Logger.save_dir
         save_path=os.path.join(dir_path,filename)
-        if os.path.exists(save_path+'.csv') and read_if_exist:
+        if os.path.exists(save_path+'.csv') and not overwrite and not Logger.loaded:
             Logger.load_logger(dir_path=dir_path,filename=filename)
 
-        combine_df = pd.DataFrame()
-        for k,v in Logger.logger_dict.items():
-            v['tag'] = k
-            combine_df = pd.concat([combine_df,v])
-        combine_df['experiment_name']=Logger.experiment_name
-
-        if overwrite_if_exist:
-            Logger.remove_history(Logger.experiment_name)
-
-        Logger.history['records']=pd.concat([combine_df,Logger.history['records']])
         Logger.history['configs'][Logger.experiment_name]=Logger.config
 
         with open(save_path+'.json','w') as f:
@@ -299,12 +286,25 @@ class Logger:
         Logger.history['records'].to_csv(save_path+'.csv',index=False)
 
     @staticmethod
-    def load_logger(dir_path=None,filename='logger_history'):
+    def load_logger(dir_path=None,filename='logger_history',overwrite=False):
         if not dir_path: dir_path = Logger.save_dir
         filepath = os.path.join(dir_path,filename)
-        Logger.history['records']=pd.read_csv(filepath+'.csv')
+        previous_history = pd.read_csv(filepath+'.csv')
         with open(filepath+'.json','r') as json_file:
             Logger.history['configs'] = json.load(json_file)
+
+        repeat_names=set(previous_history.experiment_name.unique()) & set(Logger.history['records'].experiment_name.unique())
+        if len(repeat_names) != 0 and not overwrite:
+            raise Exception(f'{repeat_names} experiment name already exist, please rename or set overwrite parameter to True.')
+        elif len(repeat_names) != 0:
+            previous_history=previous_history.loc[~previous_history.experiment_name.isin(repeat_names)]
+            Logger.history['configs'] = json.load(json_file)
+            if len(repeat_names)>0:
+                for k in Logger.history['configs'].keys():
+                    if k in repeat_names:
+                        del Logger.history['configs'][k]
+        Logger.loaded=True
+        Logger.history['records']=pd.concat([previous_history,Logger.history['records']])
 
 
 
@@ -323,11 +323,11 @@ class Logger:
 
 #example
 if __name__ == '__main__':
-    # Logger.load_logger(os.path.join(Logger.save_dir,'logger_history.pkl'))
-    Logger.init('test2')
+    Logger.load_logger()
+    Logger.init('test3')
     config = Logger.config
-    config.batch=243
-    config.lr=0.1
+    config.batch=3
+    config.lr=0.3
     config.seed=54
     training_logger=Logger('Training')#default 0
     validation_logger=Logger('Validation')#default 0
@@ -339,6 +339,10 @@ if __name__ == '__main__':
         validation_logger.check_best(category='acc',mode='max')
 
     Logger.plot(show_category=None,
+                ylim={'acc':[0,1]},
+                show=False,
+                save=True)
+    Logger.plot_experiments(show_category=None,
                 ylim={'acc':[0,1]},
                 show=False,
                 save=True)
